11b7741c998140f0a559059fe3b09df693f58e12
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-09-03 20:21:55 +0530

    committing jenkins-practice

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,10 @@
- curl -o /etc/yum.repos.d/jenkins.repo \
+growpart /dev/nvme0n1 4
+lvextend -l +70%FREE /dev/RootVG/rootVol
+lvextend -l +70%FREE /dev/RootVG/varVol
+xfs_growfs /
+xfs_growfs /var
+
+curl -o /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-21-openjdk -y
